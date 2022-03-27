5ad57c63f4cafecaff1ba5db392f7e81c37e9f7e
youngminshin <cntmin81@gmail.com>
2022-03-27 11:42:32 +0900

    springboot initialized

---
+++ src/main/java/io/github/cntmin81/restnews/RestNewsApplication.java
@@ -0,0 +1,13 @@
+package io.github.cntmin81.restnews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestNewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestNewsApplication.class, args);
+	}
+
+}
