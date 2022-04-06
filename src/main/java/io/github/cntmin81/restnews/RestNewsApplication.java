99fa6a0719e63936befa26a0836601c255f74acd
youngminshin <cntmin81@gmail.com>
2022-04-06 20:38:17 +0900

    add modelmapper

--- src/main/java/io/github/cntmin81/restnews/RestNewsApplication.java
+++ src/main/java/io/github/cntmin81/restnews/RestNewsApplication.java
@@ -1,10 +1,17 @@
 package io.github.cntmin81.restnews;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RestNewsApplication {
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestNewsApplication.class, args);
