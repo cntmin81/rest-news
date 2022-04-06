e99a64e98d2b4f1420ef1257d1a276a3ad604c25
youngminshin <cntmin81@gmail.com>
2022-04-06 21:21:01 +0900

    add update and delete controller

--- src/main/java/io/github/cntmin81/restnews/controller/NewsController.java
+++ src/main/java/io/github/cntmin81/restnews/controller/NewsController.java
@@ -3,13 +3,17 @@ package io.github.cntmin81.restnews.controller;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.xml.ws.Response;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -53,4 +57,22 @@ public class NewsController {
         return new ResponseEntity<NewsDto>(newsResponse, HttpStatus.CREATED);
     }
     
+    @PutMapping("/{id}")
+    public ResponseEntity<NewsDto> updateNews(@PathVariable(name = "id") Long id, @RequestBody NewsDto newsDto) {
+        // converto DTO to entity
+        News newsRequest = modelMapper.map(newsDto, News.class);
+
+        News news = newsService.update(id, newsRequest);
+
+        // converto entity to DTO
+        NewsDto newsResponse = modelMapper.map(news, NewsDto.class);
+
+        return ResponseEntity.ok().body(newsResponse);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteNews(@PathVariable(name = "id") Long id) {
+        newsService.delete(id);
+        return ResponseEntity.ok("ID [" + id + "] is deleted");
+    } 
 }
