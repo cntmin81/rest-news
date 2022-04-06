fbd86b356f410b81a32fffa494cec722fc8b390d
youngminshin <cntmin81@gmail.com>
2022-04-06 20:54:49 +0900

    add get one controller

--- src/main/java/io/github/cntmin81/restnews/controller/NewsController.java
+++ src/main/java/io/github/cntmin81/restnews/controller/NewsController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,6 +32,14 @@ public class NewsController {
     public List<NewsDto> getAllNews() {
         return newsService.getAll().stream().map(
             news -> modelMapper.map(news, NewsDto.class)).collect(Collectors.toList());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<NewsDto> getNewsById(@PathVariable(name = "id") Long id) {
+        News news = newsService.getOneById(id);
+
+        NewsDto newsResponse = modelMapper.map(news, NewsDto.class);
+        return ResponseEntity.ok().body(newsResponse);
     }
 
     @PostMapping
