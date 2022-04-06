3b1ffdc974e29ef913d62f2abb5985a91027ce0c
youngminshin <cntmin81@gmail.com>
2022-04-06 20:48:19 +0900

    add json format for dto

--- src/main/java/io/github/cntmin81/restnews/dto/NewsDto.java
+++ src/main/java/io/github/cntmin81/restnews/dto/NewsDto.java
@@ -2,6 +2,8 @@ package io.github.cntmin81.restnews.dto;
 
 import java.time.LocalDateTime;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import lombok.Data;
 
 @Data
@@ -9,6 +11,10 @@ public class NewsDto {
     private Long id;
     private String title;
     private String content;
-    private LocalDateTime postStartDate;
-    private LocalDateTime postEndDate;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime postStartDatetime;
+    
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime postEndDatetime;
 }
